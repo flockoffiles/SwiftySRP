e3091a4d1ce5cc962895e0a88f3eb8508cd6e520
Sergey A. Novitsky <sergey@novitsky.nl>
2017-02-09 11:26:27 +0100

    Set up workspace

---
+++ Podfile
@@ -0,0 +1,23 @@
+# TODO: Bump this up to iOS9
+platform :ios, '8.1'
+workspace 'SwiftySRP'
+
+abstract_target 'SwiftySRP_Base' do
+    use_frameworks!
+    
+    target 'SwiftySRP' do
+      project 'SwiftySRP'
+      
+      target 'SwiftySRPTests' do
+      end
+    end
+    
+    target 'SwiftySRPTestApp' do
+      project 'SwiftySRPTestApp'
+      
+      target 'SwiftySRPTestAppTests' do
+      end
+      
+    end
+end
+
---
+++ SwiftySRPTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftySRPTestApp
+//
+//  Created by Sergey A. Novitsky on 09/02/2017.
+//  Copyright © 2017 Flock of Files. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
