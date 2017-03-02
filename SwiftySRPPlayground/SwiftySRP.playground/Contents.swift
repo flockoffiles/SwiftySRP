0074e61c355231828c4b53ecaddc089ff35b5b15
Sergey A. Novitsky <sergey@novitsky.nl>
2017-03-02 09:16:00 +0100

    Work in progress

--- SwiftySRPPlayground/SwiftySRP.playground/Contents.swift
+++ SwiftySRPPlayground/SwiftySRP.playground/Contents.swift
@@ -1,8 +1,9 @@
+
 import Foundation
 //import XCPlayground
 //import SwiftySRPPlayground
 //import SwiftySRP
 
-var str = "Hello, playground"
+//var str = "Hello, playground"
 
 var anotherString = "Test"
