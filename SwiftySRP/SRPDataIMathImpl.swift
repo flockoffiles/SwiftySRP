a6ecc99e619df514025f2bbae5760218dd52c761
Sergey A. Novitsky <sergey@novitsky.nl>
2017-03-16 11:14:15 +0100

    Added imath

---
+++ SwiftySRP/SRPDataIMathImpl.swift
@@ -0,0 +1,11 @@
+//
+//  SRPDataIMathImpl.swift
+//  SwiftySRP
+//
+//  Created by Sergey A. Novitsky on 16/03/2017.
+//  Copyright © 2017 Flock of Files. All rights reserved.
+//
+
+import Foundation
+import imath
+
--- SwiftySRPPlayground/SwiftySRP.playground/Contents.swift
+++ SwiftySRPPlayground/SwiftySRP.playground/Contents.swift
@@ -1,9 +1,11 @@
-
 import Foundation
-//import XCPlayground
-//import SwiftySRPPlayground
-//import SwiftySRP
+import XCPlayground
+import SwiftySRPPlayground
+import SwiftySRP
 
 //var str = "Hello, playground"
 
-var anotherString = "Test"
+var anotherString = "Test And Test1"
+
+let secondString: String = "Check me"
+
